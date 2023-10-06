27ab6c3a45aeeba9e3b98669bbf474bc305831e7
KomarovVladimir <komarov2142@gmail.com>
2023-10-06 08:17:05 +0300

    Update init-db.sql

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -2,10 +2,10 @@ CREATE DATABASE voting;
 
 CREATE TABLE user (
 	user_id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(35),
-	last_name VARCHAR(35),
-    password VARCHAR(30),
-    creation_date TIMESTAMP,
+    first_name VARCHAR(35) NOT NULL,
+	last_name VARCHAR(35) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
 	last_access_date TIMESTAMP,
 	updated_at TIMESTAMP,
 	PRIMARY KEY (user_id)
