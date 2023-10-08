4d17a858259c551c2511697ea256905fcea20b2d
KomarovVladimir <komarov2142@gmail.com>
2023-10-08 20:11:22 +0300

    Update init-db.sql

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -1,4 +1,11 @@
+USE voting;
+
+DROP DATABASE IF EXISTS voting;
+
 CREATE DATABASE voting;
+
+#============================== user==============================
+DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
 	id INT NOT NULL AUTO_INCREMENT,
@@ -12,6 +19,45 @@ CREATE TABLE user (
 );
 
 INSERT INTO user (email, password, first_name, last_name)
-VALUES ("user@mail.com", "123123", "Name", "Surname");
+VALUES ("test@email.com", "testpassword123123", "Name", "Surname");
 
 SELECT * FROM user;
+
+#============================== room ==============================
+DROP TABLE IF EXISTS room;
+
+CREATE TABLE room (
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+	status ENUM("pending", "active", "completed"),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO room (name, status)
+VALUES
+	("Room 1", "pending"),
+    ("Room 2", "active"),
+    ("Room 3", "completed");
+    
+SELECT * FROM room;
+
+#============================== item ==============================
+DROP TABLE IF EXISTS item;
+
+CREATE TABLE item (
+	id INT NOT NULL AUTO_INCREMENT,
+    room_id INT NOT NULL,
+    name VARCHAR(30),
+	PRIMARY KEY (id),
+    FOREIGN KEY (room_id) REFERENCES room(id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+INSERT INTO item (name, room_id)
+VALUES
+	("Item 1", 1),
+    ("Item 2", 1),
+    ("Item 3", 1);
+    
+SELECT * FROM item;
