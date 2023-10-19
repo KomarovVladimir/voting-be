92d717aab6b6a36b7b7d34052adb2b3133fbcc95
KomarovVladimir <komarov2142@gmail.com>
2023-10-19 18:14:27 +0300

    Update selects.sql

--- sql/selects.sql
+++ sql/selects.sql
@@ -35,3 +35,18 @@ LEFT JOIN vote
 ON item.id = vote.item_id AND item.room_id = vote.room_id
 WHERE item.room_id = 2
 GROUP BY item.id;
+
+SELECT item.id, item.name, COUNT(vote.user_id) votes, SUM(vote.user_id = 1) AS voted
+FROM item
+LEFT JOIN vote
+ON item.id = vote.item_id AND item.room_id = vote.room_id
+WHERE item.room_id = 2
+GROUP BY item.id;
+
+
+SELECT i.id, i.name, COUNT(v.user_id) AS votes, SUM(v.user_id = 1) AS voted
+FROM item AS i
+LEFT JOIN vote AS v
+ON i.id = v.item_id AND i.room_id = v.room_id
+WHERE i.room_id = 2
+GROUP BY i.id;
